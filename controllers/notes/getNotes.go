b989702224826ea4eb41f837d69f42c5da04d593
Dev Daksan <dev.91096@gmail.com>
2020-06-09 20:44:26 +0530

    Added Get All Notes from DB

--- controllers/notes/getNotes.go
+++ controllers/notes/getNotes.go
@@ -1,7 +1,34 @@
 package notes
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/R3l3ntl3ss/Jot/models"
+	"net/http"
+)
 
 func (n Controller) GetAllNotes(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("notes"))
+
+	notes, err := n.M.GetAllNotes()
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		res := models.ErrorResponse{
+			Code:    http.StatusInternalServerError,
+			Message: "Internal Error while inserting notes into DB",
+		}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	res := models.AllNotesResponse{
+		Count: len(notes),
+		Notes: notes,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
--- models/response.go
+++ models/response.go
@@ -1,5 +1,10 @@
 package models
 
+type AllNotesResponse struct {
+	Count int     `json:"count"`
+	Notes []Notes `json:"notes"`
+}
+
 type ErrorResponse struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
